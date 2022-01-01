64e356450c7d15196e84b29e38fe2084297828e0
qrlk <hi@qrlk.me>
2022-01-01 11:55:09 +0300

    upd export scripts

--- export-gift.py
+++ export-gift.py
@@ -7,12 +7,14 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-connection = sqlite3.connect("giftmap-halloween.db")
+connection = sqlite3.connect("giftmap-2021.db")
 connection.row_factory = dict_factory
 c = connection.cursor()
 
-c.execute('''SELECT * FROM log GROUP BY gift_string;''')
+c.execute('''SELECT * FROM log WHERE typ == 'gift' GROUP BY gift_string;''')
 a = {}
 for k in c.fetchall():
-    a[int(k["gift_string"])] = {"x": k["x"], "y": k["y"], "z": k["z"]}
+    a[int(float(k["gift_string"]))] = {"x": k["x"], "y": k["y"], "z": k["z"]}
 print(json.dumps(a))
+print("")
+print(len(a))
---
+++ export-sneg.py
@@ -0,0 +1,20 @@
+import json
+import sqlite3
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+connection = sqlite3.connect("giftmap-2021.db")
+connection.row_factory = dict_factory
+c = connection.cursor()
+
+c.execute('''SELECT * FROM log WHERE typ == 'snegovik' GROUP BY gift_string;''')
+a = {}
+for k in c.fetchall():
+    a[int(float(k["gift_string"]))] = {"x": k["x"], "y": k["y"], "z": k["z"]}
+print(json.dumps(a))
+print("")
+print(len(a))
