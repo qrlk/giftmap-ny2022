1aaa69c01ed0d9a37bfc240de9dc210396506631
qrlk <hi@qrlk.me>
2022-01-01 11:54:49 +0300

    upd server

--- server.py
+++ server.py
@@ -10,17 +10,19 @@ from sanic.exceptions import NotFound
 from sanic_limiter import Limiter
 
 def get_remote_address(request):
-        return request.ip
+    return request.ip
 
-app = Sanic("giftmaph")
+app = Sanic("receiveCoords")
 limiter = Limiter(app, key_func=get_remote_address)
 
-connection = sqlite3.connect("giftmap-halloween.db")
+connection = sqlite3.connect("giftmap-2021.db")
 c = connection.cursor()
 
 c.execute('''CREATE TABLE IF NOT EXISTS "log" (
 	"id"	INTEGER,
 	"timestamp"	INTEGER,
+	"rand"	INTEGER,
+	"typ"	TEXT,
 	"ip"	TEXT,
 	"gift_string" TEXT,
 	"x"	REAL,
@@ -42,8 +44,8 @@ async def result(result):
 async def add_new_pumpkin(request, exception):
     info = json.loads(urllib.parse.unquote(request.path).replace("/", ""))
 
-    c.execute("INSERT INTO log('timestamp','ip', 'gift_string', 'x','y','z') VALUES (?,?,?,?,?,?)", (ctime(), request.ip, info["gift_string"], info["x"], info['y'], info['z']))
+    c.execute("INSERT INTO log('timestamp','ip', 'gift_string', 'x','y','z', 'typ', 'rand') VALUES (?,?,?,?,?,?,?,?)", (ctime(), request.ip, info["gift_string"], info["x"], info['y'], info['z'], info['typ'], info['rand']))
     connection.commit()
     return text("ok")
 
-app.run(host="0.0.0.0", port=1662)
+app.run(host="0.0.0.0", port=16622)
