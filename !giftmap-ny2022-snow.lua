167fa098282a9cc088f1bb338b77df3834a0aa7e
qrlk <hi@qrlk.me>
2022-01-05 15:04:19 +0300

    bump version date

--- !giftmap-ny2022-snow.lua
+++ !giftmap-ny2022-snow.lua
@@ -1,7 +1,7 @@
 require 'lib.moonloader'
 
 script_name("/giftmap-ny2022-snow")
-script_version("05.01.2022")
+script_version("05.01.2022-1")
 script_author("Serhiy_Rubin", "qrlk")
 script_properties("work-in-pause")
 script_url("https://github.com/qrlk/giftmap-ny2022")
